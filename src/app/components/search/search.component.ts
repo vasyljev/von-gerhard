763ba1f98992f0ea86f950d497bd946d34ce64ed
Mykyta <mykyta.vasyljev@gmail.com>
2019-09-06 12:51:22 +0300

    add new text

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -5,8 +5,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { DataBaseService } from '../../services/data-base.service';
 import { VetProductsItem } from '../../models/vetProductsItem';
-import { debounceTime, filter, map } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { debounceTime, filter, map, flatMap, switchMap, startWith, delay } from 'rxjs/operators';
+import { Subscription, of, NEVER, from } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -17,13 +17,21 @@ export class SearchComponent implements OnInit, OnDestroy {
 
   @Input() searchType: string;
 
-  searchSubscription: any;
+  // productSearchControl: FormControl = new FormControl();
+  // dogCheckboxControl: FormControl = new FormControl(true);
+  // catCheckboxControl: FormControl = new FormControl(true);
+  // mainSubscription: any;
+  // dogCheckboxSubscriptions:  any;
+  // catCheckboxSubscriptions:  any;
+
   searchForm: FormGroup = new FormGroup ({
     "productSearchControl": new FormControl(),
     "dogCheckboxControl": new FormControl(true),
     "catCheckboxControl": new FormControl(true)
   });
+
   subscriptions: Subscription = new Subscription();
+  searchSubscription:  any;
   dogInit: boolean = false;
   catInit: boolean = false;
 
@@ -32,17 +40,18 @@ export class SearchComponent implements OnInit, OnDestroy {
               private dataBaseService: DataBaseService) { }
 
   ngOnInit() {
-    this.subscriptions.add(
-      this.searchProduct().subscribe((result: VetProductsItem[]) => {     
-        this.localStorageService.setProductsList(result, this.searchType);
-      },
-      (err) => console.log(err),
-      () => console.log('Complite'))
-    );
+    setTimeout(()=> {
+      this.subscriptions.add(
+        this.searchProduct().subscribe((result: VetProductsItem[]) => {
+          this.localStorageService.setProductsList(result, this.searchType);
+        },
+        (err) => console.log(err),
+        () => console.log('Complite'))
+      );
+    }, 1000);
+    
    
   }
-
-
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
@@ -54,7 +63,70 @@ export class SearchComponent implements OnInit, OnDestroy {
     setTimeout(() => {
       productList = this.localStorageService.getProductsList(this.searchType);
       tmpList = this.localStorageService.getProductsList(this.searchType);
-    }, 1000);       
+    }, 500);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    // this.mainSubscription = of(this.localStorageService.getProductsList(this.searchType)).pipe(
+      
+    //   map(list => {
+    //     console.log('mainSubscription lilst', list, productList);
+    //     return list;})
+    // );
+    
+  //   this.searchSubscription = this.productSearchControl.valueChanges.pipe(
+  //     debounceTime(600),
+  //     startWith(''),
+  //     map(input => input.toLowerCase()),
+  //     switchMap(input => {
+  //       return this.mainSubscription.pipe(
+  //         map((list: VetProductsItem[]) => {
+  //           console.log('switchMap list', list);
+  //           if(!input) {
+  //             setTimeout(() => {list = tmpList;}, 650);              
+  //           };
+  //           let result = list.filter(({name}) => {
+  //             name = name.toLowerCase();
+  //             if(name.search(input) !== -1) {
+  //               return true;
+  //             } else {
+  //               return false;
+  //             }
+  //           });
+  //           console.log('input result', result);
+  //           return result;         
+  //         })
+  //       )
+  //     }),
+  //     flatMap((list: VetProductsItem[]) => this.dogCheckboxSubscriptions.pipe(     
+  //       map(value => {
+  //         if(!value) {
+  //           console.log('dogCheckbox productList false', list, productList, value)
+  //           return list.filter(({animal}) => animal === 'cat');
+  //         } else {
+  //           console.log('dogCheckbox productList true', list, productList, value);
+            
+  //           return list;
+  //         }
+  //       })
+  //     ))
+  //   );
+    
+  //   this.dogCheckboxSubscriptions = this.dogCheckboxControl.valueChanges.pipe(
+  //     startWith(true),
+  //     map(value => {
+  //       console.log('dogCheckboxSubscriptions value', value);
+  //       return value;
+  //     })
+  //   );
+
+  //   return this.searchSubscription;
+
+    
+  // };
+
+  /////////////////////////////////////////////////////////////////////
+    
     this.searchSubscription = this.searchForm.valueChanges.pipe(
       debounceTime(600),
       map(({productSearchControl, dogCheckboxControl, catCheckboxControl}) => { 
@@ -84,6 +156,8 @@ export class SearchComponent implements OnInit, OnDestroy {
         return result;
       })
     )
+
+
     return this.searchSubscription;
   }
 
