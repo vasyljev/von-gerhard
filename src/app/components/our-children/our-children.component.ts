cb0a5ef0e481ef82f33732cfdec4a07dfe17c210
Mykyta <mykyta.vasyljev@gmail.com>
2019-09-27 14:51:41 +0300

    Children animation

--- src/app/components/our-children/our-children.component.ts
+++ src/app/components/our-children/our-children.component.ts
@@ -10,6 +10,30 @@ export class OurChildrenComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.childrenAnimation('first-child', 'pre-anim-left');
+    this.childrenAnimation('second-child', 'pre-anim-right');
   }
 
+  isPartiallyVisible(element) {
+    let elementBounding = element.getBoundingClientRect();
+    let topValue = elementBounding.top;
+    let bottomValue = elementBounding.bottom;
+    let heightValue = elementBounding.height;
+    return ((topValue + heightValue >= 0) && (heightValue + window.innerHeight >= bottomValue));
+  }
+
+  childrenAnimation(childElement: string, preAnimClass: string) {
+    const elementDescription = document.querySelectorAll(`#${childElement}>.child-description>p`)[0];
+    document.addEventListener('scroll', () => {
+      const element = document.getElementById(childElement);
+      if(this.isPartiallyVisible(element)) {
+        element.classList.remove(preAnimClass);
+        element.classList.add('after-anim');
+        setTimeout(()=>{
+          elementDescription.classList.remove('pre-anim-bottom');
+          elementDescription.classList.add('after-anim');
+        }, 800);
+      }      
+    });
+  }
 }
