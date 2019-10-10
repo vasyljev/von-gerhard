14e520d6502fdaf02fb9262a4461abd731c8bb0a
Mykyta <mykyta.vasyljev@gmail.com>
2019-10-10 11:28:44 +0300

    Children lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component'; 
-import { FeedComponent } from './components/feed/feed.component';
-import { MedsComponent } from './components/meds/meds.component';
-import { AmmoComponent } from './components/ammo/ammo.component';
-import { OurChildrenComponent } from './components/our-children/our-children.component';
-import { AdultChildrenComponent } from './components/adult-children/adult-children.component';
-import { SmallChildrenComponent } from './components/small-children/small-children.component';
+// import { FeedComponent } from './components/feed/feed.component';
+// import { MedsComponent } from './components/meds/meds.component';
+// import { AmmoComponent } from './components/ammo/ammo.component';
+// import { OurChildrenComponent } from './components/our-children/our-children.component';
+// import { AdultChildrenComponent } from './components/adult-children/adult-children.component';
+// import { SmallChildrenComponent } from './components/small-children/small-children.component';
 import {ProductFullDescritionComponent} from './components/product-full-descrition/product-full-descrition.component';
 import {BasketComponent} from './components/basket/basket.component';
 import { OrdersComponent } from './components/orders/orders.component';
@@ -15,17 +15,14 @@ import { AdminGuard } from './admin.guard';
 
 
 
-const childrenRoutes: Routes =[
-  {path: 'adult', component: AdultChildrenComponent},
-  {path: 'puppies', component: SmallChildrenComponent}
-];
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'meds', loadChildren: './components/meds/meds.module#MedsModule'},
   {path: 'feed', loadChildren: './components/feed/feed.module#FeedModule'},
   {path: 'ammo', loadChildren: './components/ammo/ammo.module#AmmoModule'},
-  {path: 'children', component: OurChildrenComponent, children: childrenRoutes},  
+  {path: 'children', loadChildren: './components/our-children/our-children.module#OurChildrenModule'},  
   {path: 'full-info/:id', component: ProductFullDescritionComponent},
   {path: 'basket', component: BasketComponent},
   {path: 'orders', component: OrdersComponent, canActivate: [AdminGuard]}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { MedsComponent } from './components/meds/meds.component';
-import { FeedComponent } from './components/feed/feed.component';
-import { ListItemComponent } from './components/list-item/list-item.component';
+// import { MedsComponent } from './components/meds/meds.component';
+// import { FeedComponent } from './components/feed/feed.component';
+// import { ListItemComponent } from './components/list-item/list-item.component';
 import { DataBaseService } from './services/data-base.service';
 import { LoginService } from './services/login.service';
 import { LocalStorageService } from './services/local-storage.service';
@@ -20,16 +20,16 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { AdminAddEditProductsComponent } from './components/admin-add-edit-products/admin-add-edit-products.component';
+// import { AdminAddEditProductsComponent } from './components/admin-add-edit-products/admin-add-edit-products.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { ProductFullDescritionComponent } from './components/product-full-descrition/product-full-descrition.component';
 import { BasketComponent } from './components/basket/basket.component';
 import { OrdersComponent } from './components/orders/orders.component';
-import { SearchComponent } from './components/search/search.component';
-import { AmmoComponent } from './components/ammo/ammo.component';
-import { OurChildrenComponent } from './components/our-children/our-children.component';
-import { AdultChildrenComponent } from './components/adult-children/adult-children.component';
-import { SmallChildrenComponent } from './components/small-children/small-children.component';
+// import { SearchComponent } from './components/search/search.component';
+// import { AmmoComponent } from './components/ammo/ammo.component';
+// import { OurChildrenComponent } from './components/our-children/our-children.component';
+// import { AdultChildrenComponent } from './components/adult-children/adult-children.component';
+// import { SmallChildrenComponent } from './components/small-children/small-children.component';
 
 @NgModule({
   declarations: [
@@ -46,9 +46,9 @@ import { SmallChildrenComponent } from './components/small-children/small-childr
     OrdersComponent,
     // SearchComponent,
     // AmmoComponent,
-    OurChildrenComponent,
-    AdultChildrenComponent,
-    SmallChildrenComponent
+    // OurChildrenComponent,
+    // AdultChildrenComponent,
+    // SmallChildrenComponent
   ],
   imports: [
     BrowserModule,
