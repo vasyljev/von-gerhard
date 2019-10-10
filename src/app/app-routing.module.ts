61dcde7a3b076dadd9c12a3d580f595ed427d9ed
Mykyta <mykyta.vasyljev@gmail.com>
2019-10-10 11:41:54 +0300

    preLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component'; 
 // import { FeedComponent } from './components/feed/feed.component';
@@ -31,7 +31,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
